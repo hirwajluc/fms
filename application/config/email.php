f71f5c43d7b15da4ffa1c264ef4834cb714952a1
Hirwa Jean Luc <hirwajluc@gmail.com>
2026-06-02 12:44:36 +0200

    Revert smtp_host to ssl:// prefix — required by this server

--- application/config/email.php
+++ application/config/email.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['protocol']     = 'smtp';
-$config['smtp_host']    = 'mail.greaterproject.eu';
+$config['smtp_host']    = 'ssl://mail.greaterproject.eu';
 $config['smtp_port']    = 465;
 $config['smtp_user']    = 'no_reply@greaterproject.eu';
 $config['smtp_pass']    = 'Greater@123';
-$config['smtp_crypto']  = 'ssl';
+$config['smtp_crypto']  = '';
 $config['smtp_timeout'] = 15;
 $config['mailtype']     = 'html';
 $config['charset']      = 'utf-8';
