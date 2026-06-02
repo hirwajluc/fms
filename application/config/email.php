243919aac42007042262c76b9e7fde6766be6ee1
Hirwa Jean Luc <hirwajluc@gmail.com>
2026-06-02 12:51:07 +0200

    Switch email from SMTP to sendmail for cPanel compatibility

--- application/config/email.php
+++ application/config/email.php
@@ -1,15 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['protocol']     = 'smtp';
-$config['smtp_host']    = 'ssl://mail.greaterproject.eu';
-$config['smtp_port']    = 465;
-$config['smtp_user']    = 'no_reply@greaterproject.eu';
-$config['smtp_pass']    = 'Greater@123';
-$config['smtp_crypto']  = '';
-$config['smtp_timeout'] = 15;
-$config['mailtype']     = 'html';
-$config['charset']      = 'utf-8';
-$config['newline']      = "\r\n";
-$config['from_email']   = 'no_reply@greaterproject.eu';
-$config['from_name']    = 'GREATER FMS';
+$config['protocol']   = 'sendmail';
+$config['mailpath']   = '/usr/sbin/sendmail';
+$config['mailtype']   = 'html';
+$config['charset']    = 'utf-8';
+$config['newline']    = "\r\n";
+$config['from_email'] = 'no_reply@greaterproject.eu';
+$config['from_name']  = 'GREATER FMS';
